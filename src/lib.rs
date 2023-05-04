b7ffbb40142b4b600c4e30601901f0f9633d3887
Gandalf the grey <112687561+Harsha-vardhan-R@users.noreply.github.com>
2023-05-04 11:17:47 +0530

    most of the functions work.
    but the logic is still not implemented.

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,59 @@
-#![allow(non_snake_case)]
+#![allow(non_snake_case,unused_variables)]
 
 pub mod mlalgos {
     
     pub mod k_cluster {
-        //This function will take the entire list of centroids and also the entire dataset , returns the associated cluster list.
-        pub fn k_cluster(centroids : [[f32]] , data_set : [[f32]] ) -> [usize] {
+        use crate::n_dimen_algos::distance_between;
 
-        }
+        //This function will take the entire list of centroids and also the entire dataset , returns the associated cluster list.
+        //i.e which point is present in which cluster respectively.
+        pub fn k_cluster(centroids :  Vec<Vec<f32>> , data_set : &[&[f32]]) -> Vec<usize> {
+
+            
+            let number_of_features = centroids[0].len();
+            let number_of_clusters = centroids.len();
+            let number_of_samples = data_set.len();
+            //Just some cross checking.
+            assert!(centroids[0].len() == data_set[0].len() ,"Both the points(cluster-centroids and sample points) must have the same number of dimensions");
+
+            let mut out_associative_vec = Vec::new();
+
+            //present_nearest:
+            //we store the cluster point and it's distance , "if" after the next iteration 
+            //we found out that this point is nearer to another centroid we update the present nearest.
+            
+            //iterating through every point to see for which cluster it belongs to.
+            for sample_point in 0..number_of_samples {
+                let mut present_nearest : (usize , f32) = (1000, 9000.0);//initialising with obscure values so that this will for sure be updated.
+                //now we have one sample in our hand, time to find out the nearest centroid to this.
+                for cluster in 0..number_of_clusters {
+                    //Now we have a centroid and a sample point ;), time to to find out the distance.
+                    let dist_now = distance_between(data_set[sample_point], &centroids[cluster]);
+                    
+                    if dist_now < present_nearest.1 {
+                        present_nearest = (cluster , dist_now);
+                    }
+
+                }
+
+                //update the associative array for this point.
+                out_associative_vec.push(present_nearest.0);
+
+            }
+
+            out_associative_vec
+
+        } 
+
+
+
 
     }
 
 }
 
 pub mod n_dimen_algos {
-    //takes two arrays of length n, returns the euclidean distance between them.
+    //takes two arrays of length n(n-dimensional points), returns the euclidean distance between them.
     pub fn distance_between( point_1 : &[f32] , point_2 : &[f32] ) -> f32 {
         
         assert!(point_1.len() >= 1 && point_2.len() >= 1 , "You should have atleast 1 feature for the sample point" );
@@ -33,12 +73,59 @@ pub mod n_dimen_algos {
     this function will take all the points in a certain cluster and returns the average of the points as a point ,
     which should be updated as the new centroid for that cluster.
     */
-    pub fn give_avg_point() -> [f32] {
+    pub fn give_avg_point(samples_in_cluster : &[&[f32]]) -> () {
+
+        assert!(samples_in_cluster.len() > 0 , "The cluster must contain atleast one sample point");
+
+        let sum_of_points = 0.0;
+        let number_of_features = samples_in_cluster[0].len();
+        let number_of_sammples = samples_in_cluster.len();
+
+        
+        
+
+        
+
+    } 
+
+    //This is for individual elements this will not be needed for the user to use,
+    //takes in one of the feature of all the elements in a cluster and gives out the  
+    pub fn average(feature_in_cluster : &[f32]) -> f32 {
+
+        let mut sum = 0_f32;
+        let len = feature_in_cluster.len() as f32;
+        for i in feature_in_cluster {
+            sum += i;
+        }
+
+        sum / len
 
     }
 
-    //This is for individual elements this will not be needed for the user to use
-    pub fn average() {
+}
+
+pub mod centroid_gen {
+    use rand::prelude::*;
+
+    pub fn generate_k_centroids(number_of_clusters : usize ,
+                                number_of_features : usize ,
+                                lower_limit : f32 , 
+                                upper_limit : f32) -> Vec<Vec<f32>> 
+    {
+        let mut hava = thread_rng();
+        //creating an empty array cause now we know the size of the output.
+        let mut out_centroids:Vec<Vec<f32>> = Vec::new();
+        
+        for centroids in 0..number_of_clusters {
+            //create this point and push into the all centroids list.
+            let mut this_cluster:Vec<f32> = Vec::new();
+            for centroid_feature in 0..number_of_features {
+                this_cluster.push(hava.gen_range(lower_limit , upper_limit));
+            }
+            out_centroids.push(this_cluster);
+        }
+
+        out_centroids
 
     }
 
@@ -46,11 +133,12 @@ pub mod n_dimen_algos {
 
 
 
+
+
 #[cfg(test)]
 
 mod test {
-    use crate::n_dimen_algos::distance_between;
-
+    use crate::{n_dimen_algos::{distance_between, average}, centroid_gen::generate_k_centroids};
 
     #[test]
     fn distance() {
@@ -60,5 +148,24 @@ mod test {
 
         dbg!(distance_between(&p1, &p2));
 
-    }
+    }//working
+
+    #[test]
+    fn _average() {
+        let p1 = [0.0 , 15.0 , 0.0 , 4.0];
+
+        println!("{}", average(&p1));
+
+    }//working
+    
+    #[test]
+    fn cluster_centroid_generate() {
+
+        println!("{:?}", generate_k_centroids(5, 10, 0.0, 10.0));
+
+    }//working_perfectly!!
+
+
+
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,38 @@
-#![allow(non_snake_case)]
+#![allow(non_snake_case, unused_imports, unused_variables, non_upper_case_globals, dead_code, unused_mut)]
+use K_means_clustering::centroid_gen::generate_k_centroids;
+use rand::prelude::*;
+
+
+//Threshold for the clustering resolution.
+const THRESHOLD : f32 = 0.01;
+//the number of clusters we want to create.
+const K : usize = 2;
+//below both depend on the dataset.
+const  number_of_features : usize = 2;
+const  number_of_samples : usize = 10;
+//The below is for calculating random centroids in the range of the the dataset, 
+//to do this , normally observe the data set and choose a really leas value and a really big value from it.
+
 
 fn main() {
-    //below both depend on the dataset.
-    let number_of_features = 2;
-    let number_of_samples = 10;
-
-    //the number of clusters we want to create.
-    const k : usize = 2;
-
+    
     /*
     this array should be of the same size as the number of the sample points,
     and respectively should represent which point is related to which cluster.
     here they are k different clusters.
     */
-    let mut associated_cluster = ;
+    let mut associated_cluster : [usize ; number_of_samples] = Default::default();
 
     //given dataset to train on.
-    let dataset = ;
-    let mut k_cluster_centroid = [[f32] ; k];
+    let dataset : [[f32 ; number_of_features] ; number_of_samples] = Default::default();
+    let mut k_cluster_centroid : [[f32 ; number_of_features] ; K] = Default::default();
 
 
+    //firstly we calculate the the centroids randomly,the lower_limit and upper_limit should be manually selected, by observing the data set a little bit.
+    let centroids = generate_k_centroids(K, number_of_features, 0.0, 20.0);
 
-
+    //Now we loop calling the 
+    
 
 
 }
